982dfe055bb1bea3831bf36fd2549a9b72504664
harsweet <64262868+harsweet@users.noreply.github.com>
2023-03-25 23:44:40 -0600

    Adds script to add rateMyProf link

---
+++ scripts/content.js
@@ -0,0 +1,21 @@
+let interval = setInterval(function() {
+    const instructorDivs = document.querySelectorAll('[title="Instructor(s)"]');
+
+    instructorDivs.forEach((prevElement) => {
+        var profName = prevElement.innerText;
+        if(profName==="Staff"){
+            return;
+        }
+
+        profNameList = profName.split(" ");
+        var rateMyProfURL = "https://www.ratemyprofessors.com/search/teachers?query=" + profNameList[0] + '%20' + profNameList.slice(-1);
+        
+        const anchorElement = document.createElement('a');
+        anchorElement.href = rateMyProfURL;
+
+        anchorElement.innerHTML = prevElement.innerHTML;
+        prevElement.innerText = '';
+        prevElement.appendChild(anchorElement);
+    }); 
+  
+  }, 500);
