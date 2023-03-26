6c5cb88b090cc4ab1949499c3afa852ccca86082
harsweet <64262868+harsweet@users.noreply.github.com>
2023-03-26 01:05:58 -0600

    Adds function to add link to course outlines

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,4 @@
-let interval = setInterval(function() {
+function modifyInstructorDivs() {
     const instructorDivs = document.querySelectorAll('[title="Instructor(s)"]');
 
     instructorDivs.forEach((prevElement) => {
@@ -15,9 +15,52 @@ let interval = setInterval(function() {
 
         anchorElement.innerHTML = prevElement.innerHTML;
         anchorElement.setAttribute("target", "_blank");
-        
+
         prevElement.innerText = '';
         prevElement.appendChild(anchorElement);
     }); 
-  
-  }, 500);
+}
+
+
+function modifyCourseDivs() {
+    const courseDivs = document.querySelectorAll('.course_title');
+    const termInfo = document.querySelector('[title="Change term"]').innerText.split(" ");
+
+    const year = termInfo[0];
+    const term = termInfo[1];
+
+    let termMap = new Map([
+        ["Fall", "f"],
+        ["Winter", "w"],
+        ["Spring", "p"],
+        ["Summer", "s"]
+    ]);
+
+    courseDivs.forEach((prevElement) => {
+        var courseName = prevElement.innerText;
+        const courseNameList = courseName.split(" ");
+
+        const facultyName = (courseNameList[0]).toLowerCase();
+        const courseNumber = (courseNameList[1]).toLowerCase();
+
+        const specificUrl = facultyName + "/courses/" + termMap.get(term) + year.slice(2) + "/" + facultyName + courseNumber;
+        const courseUrl = "https://contacts.ucalgary.ca/info/" + specificUrl;
+        
+        const anchorElement = document.createElement('a');
+        anchorElement.href = courseUrl;
+
+        anchorElement.innerHTML = prevElement.innerHTML;
+        anchorElement.setAttribute("target", "_blank");
+
+        prevElement.innerText = '';
+        prevElement.appendChild(anchorElement);
+    });
+}
+
+function triggerFunctions() {
+    modifyCourseDivs();
+    modifyInstructorDivs();
+}
+
+
+let interval = setInterval(triggerFunctions, 500);
